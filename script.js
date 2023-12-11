d34d361812d5eeff252de97a3fae18772ce57c68
bostonsinaga <bostonsinga@gmail.com>
2023-12-11 17:40:17 +0700

    Using 'getPeakValley' to get style change value by y scroll

--- script.js
+++ script.js
@@ -1,111 +1,135 @@
-/** DOMs */
+//______|
+// DOMs |
+//______|
+
 const headerBackground_DOM = document.querySelector('.header-background');
 const footerBackground_DOM = document.querySelector('.footer-background');
 const profilePicture_DOM = document.querySelector('.profile-picture');
 const text_DOMs = document.querySelectorAll('.text');
 
-function forEachContents(callback) {
-    for (let i = 0; i < text_DOMs.length; i++) {
-        let contentsLength = text_DOMs[i].querySelectorAll('.content').length;
-
-        for (let j = 0; j < contentsLength; j++) {
-            const dom = text_DOMs[i].querySelector(`.content-${j}`);
-            callback(dom, i, j);
-        }
-    }
-}
+//___________|
+// FRAGMENTS |
+//___________|
 
 /**
- *  0 -- up to -> window width - profile picture width
- *  (this affected by 'scrollYPercentage')
+ * Keep value between 0 or 1 (by default prevent from zero).
+ * 
+ * @param {number} value 
+ * @param {boolean} preventZero 
+ * @returns {number} 0 to 1
  */
-let scrollY_proPicWd = 0;
+function keepInside100Percent(value, preventZero = true) {
 
-function get_text_1_width() {
-    return (window.innerWidth - scrollY_proPicWd - profilePicture_DOM.clientWidth) + 'px';
+    if (value <= 0) {
+        if (preventZero) value = 0.0001;
+        else value = 0;
+    }
+    else if (value > 1) value = 1;
+
+    return value;
 }
 
-// initialization of 'text-1' width
-const init_text_1_width_intervalId = setInterval(() => {
+class Division {
+    /**
+     * @param {*} globalFragment - scroll y fragment (range: 0 < value <= 1)
+     * @param {*} peakValue - range: 0 < value <= 1
+     * @param {*} trigonometryType - 'sin' or 'cos'
+     */
+    constructor(globalFragment = 1, peakValue = 1, trigonometryType = 'sin') {
 
-    if (profilePicture_DOM && profilePicture_DOM.clientWidth) {
+        globalFragment = keepInside100Percent(globalFragment);
+        peakValue = keepInside100Percent(peakValue);
 
-        // init the fist text ('text_DOMs[1]') contents opacities
-        forEachContents((dom, i, j) => {
-            if (i === 1) {
-                if (j === 0) {
-                    dom.style.opacity = '1';
-                }
-                else dom.style.opacity = '0';
-            }
-        });
-
-        // keep 'text_DOMs[1]' width follow the rest of available space
-        text_DOMs[1].style.width = get_text_1_width();
-
-        // stop interval
-        clearInterval(init_text_1_width_intervalId);
+        this.globalFragment = globalFragment;
+        this.peakValue = peakValue;
+        this.trigonometryType = trigonometryType;
     }
-}, 0);
+};
 
-// changeable
-let isLandscape;
+// ___________________|
+// DOMs Configuration |
+// ___________________|
+
+const bg_localFragments = [0.25, 0.75];
+const bg_division = new Division(1, 0.5, 'cos');
+
+//________|
+// EVENTS |
+//________|
+
+// 'false' mean portrait
+let isLandscape = true;
 
 function updateOrientation() {
-    isLandscape = window.innerWidth / window.innerHeight >= 1
-    ? true : false;
+    isLandscape = window.innerWidth / window.innerHeight >= 1 ? true : false;
 }
 
 updateOrientation();
+window.addEventListener('resize', updateOrientation);
 
-// fix text container size when screen size changed
-window.addEventListener('resize', () => {
-    text_DOMs[1].style.width = get_text_1_width();
-    updateOrientation();
-});
-
-// content displayed flags
-const contentDisplayedFlags = [];
-
-forEachContents((dom, i, j) => {
-    if (!contentDisplayedFlags[i]) {
-        contentDisplayedFlags.push([]);
-    }
-    
-    if (!contentDisplayedFlags[i][j]) {
-        contentDisplayedFlags[i].push(false);
-    }
-});
-
-/** EVENTS */
-
-// only returns stringified positive number
-function getFragmentInterval({
-    scrollRate,
-    fragment,
-    fragment_prev,
-    maxValue,
-    index,
-    isDecreaseToEvenNumber
+/**
+ * This using trigonometry.
+ * 
+ * @param {object} param 
+ * @param {number} param.scrollPercentage 
+ * @param {number} param.localFragments 
+ * @param {number} param.index 
+ * @param {Division} param.division 
+ * @param {boolean} param.returnInString 
+ * @returns {number} Range from 0 to 1
+ */
+function getPeakValley(param = {
+    scrollPercentage: 0,
+    localFragments: 1,
+    index: 0,
+    division: new Division(),
+    returnInString: true
 }) {
-    if (fragment <= 0) return '0';
+    param.localFragments[param.index] = keepInside100Percent(param.localFragments[param.index]);
+    const fragment = param.localFragments[param.index] * param.division.globalFragment;
 
-    let fragmentedInterval = (
-        (scrollRate - fragment_prev)
-        * 100 / (fragment * 100 * 2)
-    );
-
-    if (fragmentedInterval < 0) return '0';
-
-    // even
-    if (isLandscape &&
-        ((isDecreaseToEvenNumber && index % 2 === 0) ||
-        (!isDecreaseToEvenNumber && index % 2 !== 0))
-    ) {
-        fragmentedInterval = maxValue - fragmentedInterval;
+    if (!param.division || param.division.peakValue <= 0 || fragment === 0) {
+        console.error("Input error. The 'getPeakValley' function contains a not expected 0 or null argument.");
+        return 0;
     }
 
-    return `${fragmentedInterval}`;
+    // scroll value (0 - 1) to radian (0 - PI)
+    const scrollRadian = Math.PI * param.scrollPercentage;
+
+    // peak and valley count ('scrollRadian' = 0 is not count)
+    let period = Math.floor(scrollRadian / (Math.PI * fragment));
+
+    // decrease max period to prevent return 0 at maximum scroll
+    if (period >= param.localFragments.length) {
+        period--;
+    }
+
+    /**
+     * Only provides values for the index corresponding to the period.
+     * For example, 'bg_0' has 0 index so it has the value in period 0.
+     */
+    if (param.index !== period) {
+        return 0;
+    }
+
+    // input in radian
+    const trigonometryInput = scrollRadian / (fragment * 2);
+
+    // return value
+    let product;
+
+    if (param.division.trigonometryType === 'sin') {
+        product = Math.abs( Math.sin(trigonometryInput) * param.division.peakValue );
+    }
+    else if (param.division.trigonometryType === 'cos') {
+        product = Math.abs( Math.cos(trigonometryInput) * param.division.peakValue );
+    }
+
+    if (param.returnInString) {
+        return `${product}`;
+    }
+
+    return product;
 }
 
 window.addEventListener('scroll', () => {
@@ -115,110 +139,26 @@ window.addEventListener('scroll', () => {
     const maxScrollY = document.body.scrollHeight - window.innerHeight,
           scrollYPercentage = window.scrollY / maxScrollY;
 
-    /**
-     * Referring to 'scrollYPercentage'.
-     * Create the 0.8 as minimum and 0.9 as maximum (the rate speeding up).
-     */
-    let middleScrollYPercentage;
-
-    if (scrollYPercentage < 0.8) {
-        middleScrollYPercentage = 0;
-    }
-    else if (scrollYPercentage >= 0.8 && scrollYPercentage < 0.9) {
-        middleScrollYPercentage = (scrollYPercentage - 0.8) * 10;
-    }
-    else middleScrollYPercentage = 1;
-
-    /** BACKGROUND */
-
-    let fragmentIntervalArgs = {
-        scrollRate: scrollYPercentage,
-        maxValue: 0.5,
-        isDecreaseToEvenNumber: true
+    // object parameters initialization
+    const peakValleyParam = {
+        scrollPercentage: scrollYPercentage,
+        localFragments: 1,
+        index: 0,
+        division: new Division(),
+        returnInString: true
     };
 
-    if (window.scrollY < maxScrollY * 0.85) {
-        headerBackground_DOM.style.visibility = 'visible';
-        footerBackground_DOM.style.visibility = 'hidden';
+    if (isLandscape) {
+        /** BACKGROUNDS */
+        
+        peakValleyParam.localFragments = bg_localFragments;
+        peakValleyParam.division = bg_division;
 
-        fragmentIntervalArgs.fragment = 0.85;
-        fragmentIntervalArgs.fragment_prev = 0;
-        fragmentIntervalArgs.index = 0;
+        peakValleyParam.index = 0;
+        headerBackground_DOM.style.opacity = getPeakValley(peakValleyParam);
 
-        headerBackground_DOM.style.opacity = getFragmentInterval(fragmentIntervalArgs);
-    }
-    else {
-        headerBackground_DOM.style.visibility = 'hidden';
-        footerBackground_DOM.style.visibility = 'visible';
-
-        fragmentIntervalArgs.fragment = 0.15;
-        fragmentIntervalArgs.fragment_prev = 0.85;
-        fragmentIntervalArgs.index = 1;
-
-        footerBackground_DOM.style.opacity = getFragmentInterval(fragmentIntervalArgs);
-    }
-
-    /** TEXT */
-
-    const textFragments = [0.2, 0.8];
-
-    scrollY_proPicWd = (
-        middleScrollYPercentage *
-        (window.innerWidth - profilePicture_DOM.clientWidth)
-    );
-
-    text_DOMs[0].style.width = scrollY_proPicWd + 'px';
-    text_DOMs[1].style.width = get_text_1_width();
-
-    fragmentIntervalArgs.maxValue = 1;
-
-    for (let i = 0; i < text_DOMs.length; i++) {
-        let contentsLength = text_DOMs[i].querySelectorAll('.content').length;
-
-        for (let j = 0; j < contentsLength; j++) {
-            const dom = text_DOMs[i].querySelector(`.content-${j}`);
-
-            dom.style.transform = (
-                isLandscape && i === 1 ?
-                `scale(${1 - middleScrollYPercentage})`:
-                `scale(${middleScrollYPercentage})`
-            );
-
-            // joint change
-            if (text_DOMs[i].clientWidth >= 300) {
-
-                fragmentIntervalArgs.fragment = (1 / contentsLength) * textFragments[i];
-                fragmentIntervalArgs.fragment_prev = (j / contentsLength) * textFragments[i];
-                fragmentIntervalArgs.index = j;
-
-                const opacity = getFragmentInterval(fragmentIntervalArgs);
-                console.log(opacity);
-
-                // if (contentDisplayedFlags[i][j]) {
-                //     if (opacity > 0) {
-                //         dom.style.opacity = 0;
-                //     }
-                // }
-                // else {
-                //     contentDisplayedFlags[i][j] = true;
-                //     dom.style.opacity = opacity;
-                // }
-            }
-            // joint change
-            else if (text_DOMs[i].clientWidth <= 200) {
-                dom.style.opacity = 0;
-            }
-            // individual change
-            else dom.style.opacity = (
-                isLandscape && i === 1 ?
-                (1 - middleScrollYPercentage) / 2 :
-                middleScrollYPercentage / 2
-            );
-        }
-
-        if (i === 1) {
-            console.log('----------------------');
-        }
+        peakValleyParam.index = 1;
+        footerBackground_DOM.style.opacity = getPeakValley(peakValleyParam);
     }
 });
 
